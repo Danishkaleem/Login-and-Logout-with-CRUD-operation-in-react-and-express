b183f947be488ce8d127ccb6cdaa2f4102deb5f2
Danishkaleem <danishkaleem1122fb@gmail.com>
2024-02-15 12:46:26 +0530

    first commit

---
+++ frontend/src/pages/Root.js
@@ -0,0 +1,38 @@
+import { Outlet, useLoaderData, useSubmit } from "react-router-dom";
+
+import MainNavigation from "../components/MainNavigation";
+import { useEffect } from "react";
+import { getTokenDuration } from "../util/auth";
+
+function RootLayout() {
+  // const navigation = useNavigation();
+  const token = useLoaderData();
+  const submit = useSubmit();
+
+  useEffect(() => {
+    if (!token) {
+      return;
+    }
+    if (token === "EXPIRED") {
+      submit(null, { action: "/logout", method: "POST" });
+      return;
+    }
+    const tokenDuraton = getTokenDuration();
+    console.log(tokenDuraton);
+    setTimeout(() => {
+      submit(null, { action: "/logout", method: "POST" });
+    }, tokenDuraton);
+  }, [token, submit]);
+
+  return (
+    <>
+      <MainNavigation />
+      <main>
+        {/* {navigation.state === 'loading' && <p>Loading...</p>} */}
+        <Outlet />
+      </main>
+    </>
+  );
+}
+
+export default RootLayout;
